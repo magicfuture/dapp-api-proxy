e0ea06972752574fb4b838f7020c236ec952cc6a
magicfuture <magicfuture@gmail.com>
2022-01-22 23:14:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,39 @@
-/**
- * gatherResponse awaits and returns a response body as a string.
- * Use await gatherResponse(..) in an async function to get the response body
- * @param {Response} response
- */
- async function gatherResponse(response) {
-  const { headers } = response
-  const contentType = headers.get("content-type") || ""
-  if (contentType.includes("application/json")) {
-    return JSON.stringify(await response.json())
-  }
-  else if (contentType.includes("application/text")) {
-    return response.text()
-  }
-  else if (contentType.includes("text/html")) {
-    return response.text()
-  }
-  else {
-    return response.text()
-  }
+async function gatherResponse(response) {
+ const { headers } = response
+ const contentType = headers.get("content-type") || ""
+ if (contentType.includes("application/json")) {
+  return JSON.stringify(await response.json())
+ }
 }
 
 async function handleRequest(request) {
-  let reqHeader = {
-    headers: {
-      "content-type": "application/json;charset=UTF-8",
-    },
+ const init = {
+  method: 'POST',
+  headers: {
+    "content-type": "application/json;charset=UTF-8",
+  },
+  body: {
+    "apiKey": SERVER_API_KEY,
+    "walletAddress": walletAddress,
+    "walletEncryptionKey": walletEncryptionKey
   }
+ }
 
-  let url = new URL(request.url)
-  const { search } = url
+ let url = new URL(request.url);
+ const { search } = url;
 
-  let apiUrl = "";
-  let isMarketStack = search.indexOf("symbols");
-  let queryData = search.replace("?","");
-  queryData = queryData.replace("%40","?");
-  if(isMarketStack <= 0){
-   //Should be Crypto
-   apiUrl = "https://deep-index.moralis.io/api/v2/erc20/"+queryData+"/price?chain=eth";
-   reqHeader = {
-     headers: {
-       "content-type": "application/json;charset=UTF-8",
-       "X-API-Key": MORALIS_APPKEY
-     },
-   }
-  }else{
-   apiUrl = "http://api.marketstack.com/v1/eod/"+queryData+"&access_key="+MARKETSTACK_API_TOKEN+"&limit=1";
-  }
+ let data = await request.json()
+ let walletAddress = data.walletAddress;
+ let walletEncryptionKey = data.walletEncryptionKey;
+ let functionName = data.functionName;
+ 
+ let apiUrl = "https://dapp-locker-api.netlify.app/.netlify/functions/"+functionName;
 
-
-  // return new Response(apiUrl, reqHeader)
-  const response = await fetch(apiUrl, reqHeader)
-  const results = await gatherResponse(response)
-  return new Response(results, reqHeader)
+ const response = await fetch(apiUrl, init)
+ const results = await gatherResponse(response)
+ return new Response(results, init)
 }
 
 addEventListener("fetch", event => {
-  return event.respondWith(handleRequest(event.request))
+ return event.respondWith(handleRequest(event.request))
 })
